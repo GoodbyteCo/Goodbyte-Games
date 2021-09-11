96432815eca2cfb5759041204f65925a8998522a
Jack Guinane <jackguinane@gmail.com>
2021-09-11 14:05:57 -0700

    Add utility components

---
+++ src/components/BackButton.vue
@@ -0,0 +1,46 @@
+<template>
+	<a href="/">← Back to all games</a>
+</template>
+
+<style scoped>
+	a
+	{
+		display: block;
+		position: absolute;
+		top: 5px;
+		left: 5px;
+
+		font-family: -apple-system, BlinkMacSystemFont, Arial, Helvetica, Roboto, sans-serif;
+		font-size: 14px;
+		font-weight: 600;
+		letter-spacing: 0.02em;
+		text-transform: uppercase;
+		text-rendering: geometricPrecision;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		font-feature-settings:"kern" 1;
+		font-kerning: normal; 
+
+		color: inherit;
+		border: 2px dashed;
+		border-radius: 6px;
+		text-decoration: none;
+		padding: 10px 15px;
+
+		opacity: 0.8;
+	}
+
+	a:hover, a:focus
+	{
+		opacity: 1;
+	}
+
+	@media screen and (max-width: 600px)
+	{
+		a
+		{
+			right: 5px;
+			text-align: center;
+		}
+	}
+</style>
